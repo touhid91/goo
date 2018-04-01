68ae3f65b3382b8599e6e15c6a82ad713550adfd
Touhid Alam <touhid91@outlook.com>
2018-04-01 22:59:09 +0600

    test

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func sum(a []int, c chan int) {
+	sum := 0
+	fmt.Println("arg", a)
+	for _, v := range a {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func main() {
+	a := []int{1, 2, 3, 4, 5, 6}
+
+	c := make(chan int)
+	go sum(a[:len(a)/2], c)
+	go sum(a[len(a)/2:], c)
+
+	fmt.Println(<-c, <-c)
 }
